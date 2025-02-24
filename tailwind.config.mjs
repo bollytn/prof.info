c4bfa3be5e24a87072704e815c4cabdba48a6017
mahjoubi bilel <mahjoubi.bilel@gmail.com>
2025-02-24 12:08:43 +0100

    change font color bachground

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,10 +19,7 @@ export default {
   				DEFAULT: 'hsl(var(--popover))',
   				foreground: 'hsl(var(--popover-foreground))'
   			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
+  			primary: '#1c1c22',
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',
   				foreground: 'hsl(var(--secondary-foreground))'
@@ -32,8 +29,8 @@ export default {
   				foreground: 'hsl(var(--muted-foreground))'
   			},
   			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
+  				DEFAULT: '#00ff99',
+  				hover: '#00e187',
   			},
   			destructive: {
   				DEFAULT: 'hsl(var(--destructive))',
@@ -65,7 +62,10 @@ export default {
 		md:'768px',
 		lg:'960px',
 		xl:'1280px',
-	}
+	},
+	fontFamily:{
+		primary:"var(--font-jetbrains-mono)",
+	},
   },
   plugins: [require("tailwindcss-animate")],
 };
