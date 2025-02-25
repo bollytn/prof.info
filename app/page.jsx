2b13d5c95ac728aaafba2457aa2039bf0a9be6d5
mahjoubi bilel <mahjoubi.bilel@gmail.com>
2025-02-25 11:51:24 +0100

    photo done

--- app/page.jsx
+++ app/page.jsx
@@ -14,27 +14,27 @@ export default function Home() {
             <h1 className='h1 mb-6'>
               salut, Je suis <br /> <span className='text-accent capitalize'>mahjoubi bilel</span>
             </h1>
-            <p className='max-w-[500px] mb-9 text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio, sed ipsa aliquam error beatae earum tempora sequi odit corporis, laboriosam praesentium consequuntur eaque cum esse blanditiis asperiores quis exercitationem?</p>
-          </div>
-          {/* btn && social*/}
-          <div className='flex flex-col xl:flex-row items-center gap 8'>
-            <Button
-              variant="outline"
-              size="lg"
-              className="uppercase flex items-center gap-2">
-              <span>Télecharger CV</span>
-              <FiDownload className='text-xl' />
-            </Button>
-            <div className='mb-8 mt-8 xl:mb-8 ml-4 mr-4'>
-              <Social containerStyles='flex gap-6'
-                iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base
+            <p className='max-w-[500px] mb-9 text-white/80'>laboriosam praesentium consequuntur eaque cum esse blanditiis asperiores quis exercitationem?</p>
+            {/* btn && social*/}
+            <div className='flex flex-col xl:flex-row items-center gap 8'>
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2">
+                <span>Télecharger CV</span>
+                <FiDownload className='text-xl' />
+              </Button>
+              <div className='mb-8 mt-8 xl:mb-8 ml-8'>
+                <Social containerStyles='flex gap-6'
+                  iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base
               hover:bg-accent hover:text-primary hover:transition-all duration-500'/>
+              </div>
             </div>
           </div>
-        </div>
-        {/* photo*/}
-        <div>
-          <Photo/>
+          {/* photo*/}
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
