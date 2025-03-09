7478586f954a059556f52c035477c9242465816c
bollytn <ps3online85@gmail.com>
2025-03-09 09:38:59 +0100

    project done

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import { FiDownload } from "react-icons/fi"
 
 export default function Home() {
   return (
-    <section className='h-full'>
+    <section className='h-screen flex items-center justify-center'>
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text*/}
