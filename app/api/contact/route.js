09480672b21fcf01d96e9be40ed4c69552e17d4e
bollytn <ps3online85@gmail.com>
2025-03-11 23:32:04 +0100

    i sendgrid

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,41 +1,28 @@
 import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function POST(req) {
     const data = await req.json();
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com', // Use the correct SMTP server
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
+    const msg = {
         to: 'mahjoubi.bilel@gmail.com',
+        from: process.env.EMAIL_USER, // Use the email address or domain you verified with SendGrid
         subject: 'Portfolio',
         html: `
-      <h3>New Client</h3>
-      <p>FirstName: ${data.firstName}</p>
-      <p>LastName: ${data.lastName}</p>
-      <p>Email: ${data.email}</p>
-      <p>Télephone: ${data.phone}</p>
-      <p>Subject: ${data.subject}</p>
-      <p>Message: ${data.message}</p>
-    `
+        <h3>New Client</h3>
+        <p>FirstName: ${data.firstName}</p>
+        <p>LastName: ${data.lastName}</p>
+        <p>Email: ${data.email}</p>
+        <p>Télephone: ${data.phone}</p>
+        <p>Subject: ${data.subject}</p>
+        <p>Message: ${data.message}</p>
+        `,
     };
 
     try {
-        // Verify connection configuration
-        await transporter.verify();
-
-        // Send email
-        await transporter.sendMail(mailOptions);
-
+        await sgMail.send(msg);
         return NextResponse.json({ message: 'Message sent successfully!' }, { status: 200 });
     } catch (error) {
         console.error('Error sending email:', error);
