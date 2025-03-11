9b431b7d12e9ce546e152384b1a560fab047b42f
bollytn <ps3online85@gmail.com>
2025-03-11 23:47:36 +0100

    sendgrid update

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -6,30 +6,19 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export async function POST(req) {
     const data = await req.json();
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
+    const msg = {
         to: 'mahjoubi.bilel@gmail.com',
         from: process.env.EMAIL_USER, // Use the email address or domain you verified with SendGrid
         subject: 'Portfolio',
         html: `
-      <h3>New Client</h3>
-      <p>FirstName: ${data.firstName}</p>
-      <p>LasttName: ${data.lastName}</p>
-      <p>Email: ${data.email}</p>
-      <p>Télephone: ${data.phone}</p>
-       <p>Subject: ${data.subject}</p>
-      <p>Message: ${data.message}</p>
-    `
+        <h3>New Client</h3>
+        <p>FirstName: ${data.firstName}</p>
+        <p>LastName: ${data.lastName}</p>
+        <p>Email: ${data.email}</p>
+        <p>Télephone: ${data.phone}</p>
+        <p>Subject: ${data.subject}</p>
+        <p>Message: ${data.message}</p>
+        `,
     };
 
     try {
