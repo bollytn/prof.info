11915d9f0e19622eb104703811b9bf02c688b5f7
unknown <mahjoubi.bilel@gmail.com>
2025-04-04 09:55:19 +0100

    titre nav bar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -19,7 +19,8 @@ const MobileNav = () => {
                 <div className="mt-32 mb-40 text-center text-2xl">
                     <Link href="/">
                         <h1 className="text-4xl font-semibold">
-                            Bollytn <span className='text-accent'>.</span>
+                            <span className='text-accent'>I</span>nfo
+                            <span className='text-accent'> .</span>
                         </h1>
 
                     </Link>
