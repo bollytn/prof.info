7e9870eccec110b96e37da3ece8fa68f9fedd6e6
unknown <mahjoubi.bilel@gmail.com>
2025-04-11 18:16:46 +0100

    remove button slidePrev whene is begining

--- components/shared/WorkSliderBtn.jsx
+++ components/shared/WorkSliderBtn.jsx
@@ -1,24 +1,63 @@
-// This component is used to create a slider button for the work slider.
-// It uses the Swiper library to handle the slider functionality.
-import { useSwiper } from "swiper/react"
-import { PiCaretLeftBold, PiCaretRightBold } from 'react-icons/pi'
-import AnimatedBox from "./AnimatedBox"
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from 'react-icons/pi';
+import AnimatedBox from "./AnimatedBox";
+import { useState, useEffect } from "react";
 
 export const WorkSliderBtn = ({ containerStyles, btnStyles, iconStyles }) => {
-    const swiper = useSwiper()
+    const swiper = useSwiper();
+    const [isBeginning, setIsBeginning] = useState(true); // Track if at the beginning
+    const [isEnd, setIsEnd] = useState(false); // Track if at the end
+
+    useEffect(() => {
+        // Update button visibility on slide change
+        const handleSlideChange = () => {
+            setIsBeginning(swiper.isBeginning);
+            setIsEnd(swiper.isEnd);
+        };
+
+        // Attach event listeners
+        swiper.on('slideChange', handleSlideChange);
+        swiper.on('reachBeginning', () => setIsBeginning(true));
+        swiper.on('reachEnd', () => setIsEnd(true));
+        swiper.on('fromEdge', () => {
+            setIsBeginning(swiper.isBeginning);
+            setIsEnd(swiper.isEnd);
+        });
+
+        // Cleanup event listeners on unmount
+        return () => {
+            swiper.off('slideChange', handleSlideChange);
+            swiper.off('reachBeginning');
+            swiper.off('reachEnd');
+            swiper.off('fromEdge');
+        };
+    }, [swiper]);
+
     return (
         <div className={containerStyles}>
-            <AnimatedBox>
-                <button className={`${btnStyles} btn-animate`}
-                    onClick={() => swiper.slidePrev()}>
-                    <PiCaretLeftBold className={iconStyles} />
-                </button>
-            </AnimatedBox>
-            <AnimatedBox>
-                <button className={`${btnStyles} btn-animate`} onClick={() => swiper.slideNext()}>
-                    <PiCaretRightBold className="iconStyles" />
-                </button>
-            </AnimatedBox>
+            {/* Previous Button */}
+            {!isBeginning && (
+                <AnimatedBox>
+                    <button
+                        className={`${btnStyles} btn-animate`}
+                        onClick={() => swiper.slidePrev()}
+                    >
+                        <PiCaretLeftBold className={iconStyles} />
+                    </button>
+                </AnimatedBox>
+            )}
+
+            {/* Next Button */}
+            {!isEnd && (
+                <AnimatedBox>
+                    <button
+                        className={`${btnStyles} btn-animate`}
+                        onClick={() => swiper.slideNext()}
+                    >
+                        <PiCaretRightBold className={iconStyles} />
+                    </button>
+                </AnimatedBox>
+            )}
         </div>
-    )
-}
+    );
+};
