e61eaeff310b964175e26d7b0ae22bd9fa854f86
unknown <mahjoubi.bilel@gmail.com>
2025-04-12 08:25:19 +0100

    add animation slide

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -5,6 +5,7 @@ import { SlideTabsExample } from '@/components/SlideTabsExample';
 import { motion, useScroll, useTransform, useSpring } from 'framer-motion';
 import Image from 'next/image';
 import AnimatedContent from '@/components/shared/AnimatedContent';
+import { Reveal } from '@/components/shared/Reveal'; // Import the Reveal component
 
 const gradient = (mask) =>
     `conic-gradient(black 0%, black ${mask ? 0 : 100}%, transparent ${mask ? 0 : 100}%, transparent 100%)`;
@@ -34,7 +35,7 @@ const ScrollImage = ({ project }) => {
             <Image
                 src={project.image}
                 alt={project.title}
-                sizes='100vw'
+                sizes="100vw"
                 fill
                 className="object-cover transition-transform duration-500 group-hover:scale-110"
             />
@@ -88,6 +89,7 @@ const Work = () => {
                                 <AnimatedContent key={index}>
                                     <div className="relative group mb-2 p-6 bg-gray-800 rounded-lg shadow-lg transition-all duration-300 hover:shadow-2xl hover:scale-105 hover:bg-gray-700">
                                         {/* Divider Line */}
+
                                         <h3 className="flex mb-4 items-center w-full">
                                             <span className="flex-grow bg-yellow-300 shadow-[0_0_5px_yellow] rounded h-px"></span>
                                             <span className="uppercase mx-3 text-lg font-medium text-accent/60 animate-bounce">
@@ -96,41 +98,50 @@ const Work = () => {
                                             <span className="flex-grow bg-yellow-300 shadow-[0_0_5px_yellow] rounded h-px"></span>
                                         </h3>
 
+
                                         {/* Project Title */}
-                                        <h2 className="text-4xl font-bold leading-none text-white capitalize group-hover:text-accent transition-all duration-300">
-                                            {project.title} - {selectedClass}
-                                        </h2>
+                                        <Reveal>
+                                            <h2 className="text-4xl font-bold leading-none text-white capitalize group-hover:text-accent transition-all duration-300">
+                                                {project.title} - {selectedClass}
+                                            </h2>
+                                        </Reveal>
 
                                         {/* Project Description */}
-                                        <p className="text-white/60 mt-4 group-hover:text-white transition-all duration-300">
-                                            {project.description}
-                                        </p>
+                                        <Reveal>
+                                            <p className="text-white/60 mt-4 group-hover:text-white transition-all duration-300">
+                                                {project.description}
+                                            </p>
+                                        </Reveal>
 
                                         {/* Project Stack */}
-                                        <ul className="flex flex-wrap gap-4 mt-4 text-left">
-                                            {project.stack.map((item, idx) => (
-                                                <li
-                                                    key={idx}
-                                                    className="text-accent group-hover:text-yellow-300 transition-all duration-300"
-                                                >
-                                                    {item.name}
-                                                </li>
-                                            ))}
-                                        </ul>
+                                        <Reveal>
+                                            <ul className="flex flex-wrap gap-4 mt-4 text-left">
+                                                {project.stack.map((item, idx) => (
+                                                    <li
+                                                        key={idx}
+                                                        className="text-accent group-hover:text-yellow-300 transition-all duration-300"
+                                                    >
+                                                        {item.name}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </Reveal>
 
                                         {/* Project Image with Scroll Animation */}
                                         <ScrollImage project={project} />
 
                                         {/* Project Link */}
-                                        <a
-                                            href={project.live}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="mt-4 inline-block text-accent group-hover:text-yellow-300 transition-all duration-300 relative"
-                                        >
-                                            <span className="absolute inset-0 scale-0 group-hover:border-b-2 group-hover:border-yellow-300 group-hover:scale-100 opacity-50 transition-transform duration-300 rounded-lg"></span>
-                                            <span className="relative z-10 ">Télécharger</span>
-                                        </a>
+                                        <Reveal>
+                                            <a
+                                                href={project.live}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="mt-4 inline-block text-accent group-hover:text-yellow-300 transition-all duration-300 relative"
+                                            >
+                                                <span className="absolute inset-0 scale-0 group-hover:border-b-2 group-hover:border-yellow-300 group-hover:scale-100 opacity-50 transition-transform duration-300 rounded-lg"></span>
+                                                <span className="relative z-10 ">Télécharger</span>
+                                            </a>
+                                        </Reveal>
                                     </div>
                                 </AnimatedContent>
                             ))}
