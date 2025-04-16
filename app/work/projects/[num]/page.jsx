0b1ebaf2ec020145f822ab80c0ccd7fa21c3d9aa
unknown <mahjoubi.bilel@gmail.com>
2025-04-16 12:28:35 +0100

    add ds theorique

--- app/work/projects/[num]/page.jsx
+++ app/work/projects/[num]/page.jsx
@@ -146,6 +146,23 @@ const ProjectPage = async ({ params }) => {
                     ))}
                 </div>
             )}
+
+            {/* Display all images for project 9 */}
+            {num === "09" && (
+                <div className="flex flex-row flex-wrap justify-center items-center gap-4 mt-2">
+                    {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
+                        <AnimatedContent key={index} className="relative w-full max-w-4xl h-auto rounded-lg overflow-hidden shadow-md shadow-accent-hover">
+                            <Image
+                                src={`/assets/work/8/dstheorique/cour${index}.png`} // Dynamically load images
+                                alt={`Project 1 Image ${index}`}
+                                width={1920} // Full width for large screens
+                                height={1080} // Maintain aspect ratio
+                                className="rounded-lg shadow-lg"
+                            />
+                        </AnimatedContent>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
