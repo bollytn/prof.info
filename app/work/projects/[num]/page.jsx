4e1c91df34d8456bfbf9755fb5191a8d2a28b364
unknown <mahjoubi.bilel@gmail.com>
2025-04-16 16:18:59 +0100

    add ds pratique

--- app/work/projects/[num]/page.jsx
+++ app/work/projects/[num]/page.jsx
@@ -163,6 +163,23 @@ const ProjectPage = async ({ params }) => {
                     ))}
                 </div>
             )}
+
+            {/* Display all images for project 10 */}
+            {num === "10" && (
+                <div className="flex flex-row flex-wrap justify-center items-center gap-4 mt-2">
+                    {[1, 2, 3, 4, 5].map((index) => (
+                        <AnimatedContent key={index} className="relative w-full max-w-4xl h-auto rounded-lg overflow-hidden shadow-md shadow-accent-hover">
+                            <Image
+                                src={`/assets/work/8/dspratique/cour${index}.png`} // Dynamically load images
+                                alt={`Project 1 Image ${index}`}
+                                width={1920} // Full width for large screens
+                                height={1080} // Maintain aspect ratio
+                                className="rounded-lg shadow-lg"
+                            />
+                        </AnimatedContent>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
