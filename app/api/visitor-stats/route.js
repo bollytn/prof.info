41bd3e2c11ba4c64d57ef5982ed4fb32d41ffaa1
unknown <mahjoubi.bilel@gmail.com>
2025-04-26 23:54:11 +0100

    add route visitor

---
+++ app/api/visitor-stats/route.js
@@ -0,0 +1,23 @@
+let visitorCount = 60; // Nombre de visites aujourd'hui
+let topDay = { count: 500}; // Top visites par jour
+let onlineVisitors = 5; // Nombre de visiteurs en ligne
+
+export async function GET(request) {
+    // Simuler une augmentation des visiteurs en ligne
+    onlineVisitors = Math.floor(Math.random() * 50) + 5;
+
+    // Simuler une augmentation des visites aujourd'hui
+    visitorCount += Math.floor(Math.random() * 1);
+
+    return new Response(
+        JSON.stringify({
+            today: visitorCount,
+            topDay,
+            online: onlineVisitors,
+        }),
+        {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        }
+    );
+}
