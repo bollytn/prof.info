9293c801980a05caf2c43bd199422209b3403d90
unknown <mahjoubi.bilel@gmail.com>
2025-04-28 00:04:50 +0100

    close menu autoatic

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -5,24 +5,29 @@ import Link from "next/link"
 import { CiMenuFries } from 'react-icons/ci'
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import Links from "@/components/data/links"
-import { motion } from "framer-motion"
+import { useState } from "react"
 
 const MobileNav = () => {
     const path = usePathname()
+    const [isOpen, setIsOpen] = useState(false) // State to control the menu visibility
+
+    const handleClose = () => {
+        setIsOpen(false) // Close the menu
+    }
+
     return (
-        <Sheet>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger className="flex items-center justify-center">
                 <CiMenuFries className="text-[32px] text-accent" />
             </SheetTrigger>
             <SheetContent className='flex flex-col'>
                 {/* logo */}
                 <div className="mt-32 mb-40 text-center text-2xl">
-                    <Link href="/">
+                    <Link href="/" onClick={handleClose}>
                         <h1 className="text-4xl font-semibold">
                             <span className='text-accent'>I</span>nfo
                             <span className='text-accent'> .</span>
                         </h1>
-
                     </Link>
                 </div>
 
@@ -33,6 +38,7 @@ const MobileNav = () => {
                             <Link
                                 key={index}
                                 href={link.path}
+                                onClick={handleClose} // Close the menu when a link is clicked
                                 className={`${path === link.path &&
                                     'text-accent border-b-2 border-accent'} 
                                      text-lg capitalize hover:text-accent transition-all`}
