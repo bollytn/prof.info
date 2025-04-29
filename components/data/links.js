51bb0da3ae52a5043edc8f5d51cd3e5377f113e6
unknown <mahjoubi.bilel@gmail.com>
2025-04-29 09:39:15 +0100

    add cv

--- components/data/links.js
+++ components/data/links.js
@@ -1,7 +1,7 @@
 const links = [
     { name: 'Accueil', path: '/' },
     { name: 'Services', path: '/service' },
-   // { name: 'Curriculum vitae', path: '/resume' },
+    { name: 'Curriculum vitae', path: '/resume' },
     { name: 'Cours', path: '/work' },
    // { name: 'Contact', path: '/contact' },
 ]
