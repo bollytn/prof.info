c0b96153bafed62511b1e97fa47c9542261ceab5
unknown <mahjoubi.bilel@gmail.com>
2025-04-29 09:38:04 +0100

    remove contact and cv

--- components/data/links.js
+++ components/data/links.js
@@ -1,8 +1,8 @@
 const links = [
     { name: 'Accueil', path: '/' },
     { name: 'Services', path: '/service' },
-  //  { name: 'Curriculum vitae', path: '/resume' },
+   // { name: 'Curriculum vitae', path: '/resume' },
     { name: 'Cours', path: '/work' },
-    { name: 'Contact', path: '/contact' },
+   // { name: 'Contact', path: '/contact' },
 ]
 export default links;
