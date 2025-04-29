8fe3cf9b1c667aec18687c6f4329c54bc3b3825e
unknown <mahjoubi.bilel@gmail.com>
2025-04-29 08:51:31 +0100

    capitalise position

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -159,7 +159,7 @@ const Resume = () => {
                                                 <AnimatedContent key={index}>
                                                     <li className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
                                                         <span className="text-accent">{item.duration}</span>
-                                                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                                                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left capitalize">
                                                             {item.position}
                                                         </h3>
                                                         <div className="flex items-center gap-3">
