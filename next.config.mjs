e4cc1ad5384440f186d234c5dc77b4be0558b3da
unknown <mahjoubi.bilel@gmail.com>
2025-05-06 19:08:43 +0100

    config.next

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ const nextConfig = {
     reactStrictMode: false,
     images: {
         domains: ['counter4.optistats.ovh'], // Ajoutez le domaine ici
-      },
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'prof-info.vercel.app',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 export default nextConfig;
