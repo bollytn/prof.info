171f1c9d6be2e0d74eaf2d9f6a154eef792fc63e
mahjoubi.bilel <mahjoubi.bilel@gmail.com>
2025-05-07 13:23:56 +0000

    update img by Image in scrollImage

--- components/shared/ScrollImage.jsx
+++ components/shared/ScrollImage.jsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
+
 const ScrollImage = ({ project }) => {
     return (
         <div className="mt-4">
-            <img
+            <Image
                 src={project.image}
                 alt={project.title}
                 className="rounded-lg shadow-lg w-full"
+                width={1920}
+                height={1080}
+                loading="lazy"
+                unoptimized
             />
         </div>
     );
