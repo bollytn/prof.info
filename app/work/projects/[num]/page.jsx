d5c16a3b4a1d5c7f4cfbeb07e1b9d2c708dec5b5
unknown <mahjoubi.bilel@gmail.com>
2025-05-10 18:18:15 +0100

    replace src by server

--- app/work/projects/[num]/page.jsx
+++ app/work/projects/[num]/page.jsx
@@ -120,7 +120,7 @@ const renderContent = (num, project) => {
                     </video>
                     {/* Iframe */}
                     <iframe
-                        src="https://forkphorus.github.io/embed.html#http://localhost:3000/assets/work/9/projets/projet1/interactif%20carte%20tunisienne%20finish.sb3"
+                        src="https://forkphorus.github.io/embed.html#https://raw.githubusercontent.com/bollytn/prof.info/main/public/assets/work/9/projets/projet1/interactif%20carte%20tunisienne%20finish.sb3"
                         width="100%"
                         height="700px"
                         className="border-none w-full"
