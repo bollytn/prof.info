a96ba9ee167060aa682dceb4564fad088474c1ee
mahjoubi.bilel <mahjoubi.bilel@gmail.com>
2025-05-19 14:43:12 +0000

    ajouter index par html

--- app/layout.js
+++ app/layout.js
@@ -56,7 +56,7 @@ export default function RootLayout({ children }) {
             }),
         }}
     />
-
+<meta name="google-site-verification" content="lQIAE2l1VFTP--g_QyRAInTPixbb3q9hAg8N_vbREIk" />  
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
